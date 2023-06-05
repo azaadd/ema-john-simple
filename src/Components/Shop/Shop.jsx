dde167e8387f05714deedb4083bc147ef62ae9fa
azaadd <abulkalam.kta@gmail.com>
2023-06-05 23:13:23 +0600

    added card price total

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
 
@@ -14,9 +15,31 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
+    useEffect( () =>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id of the added Product
+        for(const id in storedCart){
+            //step 2: get product from products state by using id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                //step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the added product to the saved cart
+                savedCart.push(addedProduct);
+            }
+            console.log('added Product', addedProduct);
+        }
+        // step 5:set the cart
+        setCart(savedCart);
+
+    }, [products]);
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
